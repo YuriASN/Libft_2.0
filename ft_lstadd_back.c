14d5cf68b21325f258ecc2ef3e1f859d20f1eb3a
Yuri ASN <yuriasn@hotmail.com>
2025-10-28 15:45:25 +0000

    Fixed when list has no nodes, setting now the new node as first

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -8,14 +8,10 @@
  * The address of a pointer to the node to be added. */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	int		i;
-	t_list	*tmp;
-
-	if (!lst || !lst[0])
+	if (!lst)
 		return ;
-	i = -1;
-	tmp = lst[0];
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
 }
