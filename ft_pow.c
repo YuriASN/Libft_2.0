3a490dd181598b9cc5a1869fe592e5a2cb8862c4
ysantos- <83174271+YuriASN@users.noreply.github.com>
2026-02-11 02:06:46 +0000

    Refactor ft_pow to improve handling of powers
    Refactor ft_pow function to handle negative powers by returning 0. Initialize result to 1 and simplify the loop for positive powers.

--- ft_pow.c
+++ ft_pow.c
@@ -4,18 +4,14 @@
  * @param base Base number.
  * @param power Power number.
  * @return Result of equation. */
-int	ft_pow(int base, int power)
+size_t	ft_pow(int base, int power)
 {
 	int	result;
 
-	if (!power || base == 1)
-		return (1);
-	result = base;
-	if (power > 0)
-		while (power--)
-			result *= base;
 	if (power < 0)
-		while (power++ < 1)
-			result /= base;
+		return (0);
+	result = 1;
+	while (power--)
+		result *= base;
 	return (result);
 }
