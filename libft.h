42ac0ef4e1eeaeb2d13dc07e68fe728ddbc0def3
ysantos- <83174271+YuriASN@users.noreply.github.com>
2026-02-11 02:15:24 +0000

    Change return type of ft_pow to ssize_t

--- libft.h
+++ libft.h
@@ -62,7 +62,7 @@ int		ft_atoi(const char *str);
 int		ft_abs(int nbr);
 char	*ft_itoa(int n);
 void	ft_intswap(int *x, int *y);
-size_t	ft_pow(int base, int power);
+ssize_t	ft_pow(int base, int power);
 
 //Lists
 
